fef9b4e480430f27aba8a777158b1ea9aa0dbd0f
Zai Santillan <zaisantillan14@gmail.com>
2022-12-08 22:04:54 +0800

    feat: add day 21

---
+++ 03-Guess the Flag/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,16 @@
+//  Date: 12/7/22
+//
+//  Author: Zai Santillan
+//  Github: @plskz
+
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
