34aa6ec87f4d8c4b26cdbd7f084c0a8d8eb53262
Zai Santillan <zaisantillan14@gmail.com>
2023-02-28 15:06:48 +0800

    feat(Moonshot): add challenge 2

---
+++ 11-Moonshot/Moonshot/Moonshot/Util/CustomDivider.swift
@@ -0,0 +1,22 @@
+//  Date: 2/28/23
+//
+//  Author: Zai Santillan
+//  Github: @plskz
+
+
+import SwiftUI
+
+struct CustomDivider: View {
+    var body: some View {
+        Rectangle()
+            .frame(height: 2)
+            .foregroundColor(.lightBackground)
+            .padding(.vertical)
+    }
+}
+
+struct CustomDivider_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomDivider()
+    }
+}
